ae571ddff2abc1c130bb0dcfdcea878a6faefc80
Igor Boyko <igor.a.boyko@gmail.com>
2019-12-02 15:08:17 +0300

    Initial Commit

--- study_program.py
+++ study_program.py
@@ -1,1 +1,9 @@
+class Car():
+    """docstring for ."""
 
+    def __init__(self, speed,color):
+#        super(, self).__init__()
+        self.speed=speed
+        self.colore=color
+
+print("Hello%run "/home/igor/Desktop/work/study/stu%run "/home/igor/Desktop/work")
